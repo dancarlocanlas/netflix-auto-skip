8eb1317f5a056474d4e2c3848a39574d3f7fde29
dc <dancarlocanlas@gmail.com>
2021-02-14 20:11:08 +0800

    chore: added JSDocs

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
+/**
+ * Listens for tab changes -- specifically URL changes
+ * then triggers the DOM observer via messaging
+ */
 chrome.tabs.onUpdated.addListener((tabId, _, tabInfo) => {
   const { url } = tabInfo;
 
   if (!url.includes('netflix.com/watch')) return;
+
+  console.log('url changed for a netflix tab')
   
   chrome.tabs.sendMessage(tabId, { urlChange: true });
 });
--- content.js
+++ content.js
@@ -1,21 +1,42 @@
+/**
+ * Check for the Skip Intro / Recap button
+ * then trigger the click event.
+ */
+
 const SKIP_INTRO = 'Skip Intro';
 const SKIP_RECAP = 'Skip Recap';
 const getNode = (target) => document.querySelector(`[aria-label="${target}"]`);
 const observerConfig = { childList: true, subtree: true };
 
+/**
+ * Mutation observer callback function 
+ * @param {Array} _ mutations (unused)
+ * @param {Object} observerInstance current observer instance
+ */
 function observerCallback(_, observerInstance) {
+  console.log('Running observer...');
   const targetNode = getNode(SKIP_INTRO) || getNode(SKIP_RECAP);
     
   if (!targetNode) return;
 
+  console.log('Target node found!');
   targetNode.click();
+
+  // In cases that there's an intro after a recap
+  if (targetNode.getAttribute('aria-label') === SKIP_RECAP) return;
+
+  console.log('Disconnecting observer...');
   observerInstance.disconnect();
 }
 
+/**
+ * Listen for messages from background script
+ */
 chrome.runtime.onMessage.addListener((message) => {
   if (message.urlChange) {
     const observer = new MutationObserver(observerCallback);
     
+    console.log('Starting observer...');
     observer.observe(document.body, observerConfig);
   }
 })
